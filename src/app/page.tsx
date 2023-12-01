d342ca2d2cd98046ad6b403b4563cfe6be5f29ca
TienPhuc02 <dtp2352002@gmail.com>
2023-12-01 09:56:34 +0700

    fetch data homepage , fetch data server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import MainContentHomePage from "@/components/homepage/main-content/index.main";
 import { Container } from "@mui/material";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      category: "CHILL",
+      limit: 10,
+    }),
+  });//axios
+  console.log("🚀 ~ file: page.tsx:15 ~ HomePage ~ res:", await res.json());
   return (
     <div className="home-page-main">
       <div className="main-content-slider">
