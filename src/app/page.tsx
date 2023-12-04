bf8cbf42c8d37d51ad06e0ea2cac578748b2c8b7
TienPhuc02 <dtp2352002@gmail.com>
2023-12-04 09:40:10 +0700

    fetch wrapper JS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
 import MainContentHomePage from "@/components/homepage/main-content/index.main";
 import { Container } from "@mui/material";
-
+import { sendRequestJS } from "@/utils/old.api";
 export default async function HomePage() {
-  const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+  // const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     category: "CHILL",
+  //     limit: 10,
+  //   }),
+  // });//axios
+  // console.log("🚀 ~ file: page.tsx:15 ~ HomePage ~ res:", await res.json());
+  const res = await sendRequestJS({
+    url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      category: "CHILL",
-      limit: 10,
-    }),
-  });//axios
-  console.log("🚀 ~ file: page.tsx:15 ~ HomePage ~ res:", await res.json());
+    body: { category: "CHILL", limit: 10 },
+  });
+  console.log("🚀 ~ file: page.tsx:20 ~ HomePage ~ res:", res)
   return (
     <div className="home-page-main">
       <div className="main-content-slider">
